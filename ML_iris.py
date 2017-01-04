6b0f28bc4388c2ec56422de1d8a354babe2c0f26
j1mehta <jatinmehta.jm@gmail.com>
2017-01-03 19:46:01 -0800

    Add files via upload
    Iris dataset cross-validated via: LogisticRegression, LinearDiscriminantAnalysis, KNeighborsClassifier, DecisionTreeClassifier, GaussianNB

---
+++ ML_iris.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 03 18:13:42 2017
+
+@author: j.mehta
+"""
+#%%
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import scatter_matrix
+import pandas as pd
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+#%%
+
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+df = pd.read_csv(url, names=names)
+# box and whisker plots
+#df.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+#plt.show()
+
+#df.hist()
+#plt.show()
+
+
+
+# scatter plot matrix
+#%%
+scatter_matrix(df)
+plt.show()
+#%%
+
+#%%
+array = df.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = cross_validation.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+#%%
+
+#%%
+num_folds = 10
+num_instances = len(X_train)
+seed = 7
+scoring = 'accuracy'
+
+#%%
+
+#%%
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+#%%
+for name, model in models:
+	kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
+	cv_results = cross_validation.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+#%%
